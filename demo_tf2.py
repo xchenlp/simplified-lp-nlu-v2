e6b5b197e7611c20d187d4c90262fe498c8f410a
Jian Wang <jianwolf@umich.edu>
2020-03-18 16:48:02 -0700

    trying tf 2.0 to better extract the tf model

---
+++ demo_tf2.py
@@ -0,0 +1,40 @@
+from model import Model
+import os
+from pdb import set_trace
+import pandas
+from sklearn.metrics import classification_report
+
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '-1'  # disable gpu. This is because on ca-gpu2, the cuDNN version is wrong for tensorflow
+
+
+def main():
+    model = Model(word2vec_pkl_path='/data/cb_nlu_v2/vectors/wiki-news-300d-1M.pkl', config_path='config.yml')
+
+    test_model_path = '/data/cb_nlu_test_model'
+    tr_set_path = '/data/deep-sentence-classifiers/preprocessed_data/Hawaiian/tr.json'
+    te_set_path = '/data/deep-sentence-classifiers/preprocessed_data/Hawaiian/te.json'
+
+    ######################### training #########################
+    model.train(tr_set_path, test_model_path)
+    ######################### training ends #########################
+
+    ######################### loading #########################
+    model.load(test_model_path)
+    ######################### loading ends #########################
+
+    ######################### predicting #########################
+    df_te = pandas.read_json(te_set_path, lines=True)
+    output = model.predict(list(df_te.text))
+    ######################### predicting ends #########################
+
+    ######################### evaluating the prediction #########################
+    ground_truths = list(df_te.intent)
+    predictions = [x['label'] for x in output]
+    threshold_predictions = [x['label'] if x['highestProb'] > 0.7 else 'undefined' for x in output]
+    print(classification_report(y_true=ground_truths, y_pred=threshold_predictions))
+    ######################### evaluating the prediction ends #########################
+
+
+if __name__ == '__main__':
+    main()
---
+++ model_tf2.py
@@ -0,0 +1,249 @@
+from sklearn import preprocessing
+from random import shuffle
+import numpy as np
+import collections
+
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Dropout, Activation, Conv1D, GlobalMaxPooling1D
+from tensorflow.keras.models import Sequential, model_from_json
+from tensorflow.keras import backend as K
+from gensim.models.keyedvectors import KeyedVectors
+from nltk.tokenize import TreebankWordTokenizer
+import re
+import pickle
+import os
+
+import yaml
+import pandas
+from typing import List
+
+
+def read_csv_json(file_name) -> pandas.DataFrame:
+    if file_name.endswith('json') or file_name.endswith('jsonl'):
+        df = pandas.read_json(file_name, lines=True)
+    elif file_name.endswith('csv'):
+        df = pandas.read_csv(file_name)
+    else:
+        raise NotImplementedError
+    return df
+
+
+def use_only_alphanumeric(input):
+    pattern = re.compile('[\W^\'\"]+')
+    output = pattern.sub(' ', input).strip()
+    return output
+
+
+def tokenize_and_vectorize(tokenizer, embedding_vector, dataset):
+    vectorized_data = []
+    # probably could be optimized further
+    ds1 = [use_only_alphanumeric(samp) for samp in dataset]
+    token_list = [tokenizer.tokenize(sample) for sample in ds1]
+
+    unk_vec = None
+    try:
+        unk_vec = embedding_vector['UNK'].tolist()
+    except Exception as e:
+        pass
+
+    for tokens in token_list:
+        vecs = []
+        for token in tokens:
+            try:
+                vecs.append(embedding_vector[token].tolist())
+            except KeyError:
+                print('token not found: (%s) in sentence: %s' % (token, ' '.join(tokens)))
+                if unk_vec is not None:
+                    vecs.append(unk_vec)
+                continue
+        vectorized_data.append(vecs)
+    return vectorized_data
+
+
+def pad_trunc(data, maxlen):
+    """
+    For a given dataset pad with zero vectors or truncate to maxlen
+    """
+    new_data = []
+    # Create a vector of 0s the length of our word vectors
+    zero_vector = []
+    for _ in range(len(data[0][0])):
+        zero_vector.append(0.0)
+
+    for sample in data:
+        if len(sample) > maxlen:
+            temp = sample[:maxlen]
+        elif len(sample) < maxlen:
+            temp = list(sample)
+            # Append the appropriate number 0 vectors to the list
+            additional_elems = maxlen - len(sample)
+            for _ in range(additional_elems):
+                temp.append(zero_vector)
+        else:
+            temp = sample
+        new_data.append(temp)
+    return new_data
+
+
+class Model:
+    def __init__(self, word2vec_pkl_path, config_path):
+        with open(config_path, 'r') as f:
+            self.model_cfg = yaml.safe_load(f)['model']
+        self.tokenizer = TreebankWordTokenizer()
+
+        with open(word2vec_pkl_path, 'rb') as f:
+            self.vectors = pickle.load(f)
+        self.model = None
+        self.session = None
+        self.graph = None
+        self.le_encoder = None
+
+    def train(self, tr_set_path, save_path):
+        """
+        Train a model for a given dataset
+        Dataset should be a list of tuples consisting of
+        training sentence and the class label
+        """
+        df_tr = read_csv_json(tr_set_path)
+        messages = list(df_tr.text)
+        labels = list(df_tr.intent)
+        dataset = [{'data': messages[i], 'label': labels[i]} for i in range(len(df_tr))]
+
+        (x_train, y_train, le_encoder) = self.__preprocess(dataset)
+        model = self.__build_model(num_classes=len(le_encoder.classes_))
+
+        print('start training')
+        model.fit(x_train, y_train,
+                    batch_size=self.model_cfg['batch_size'],
+                    epochs=self.model_cfg['epochs'])
+        print('finished training')
+        self.model = model
+        self.le_encoder = le_encoder
+        self.save(save_path)
+
+    def save(self, path):
+        '''
+        save model based on model, encoder
+        '''
+
+        if not os.path.exists(path):
+            os.makedirs(path, exist_ok=True)
+        print(f'saving model to {path}')
+        structure_file = os.path.join(path, 'structure.json')
+        weight_file = os.path.join(path, 'weight.h5')
+        labels_file = os.path.join(path, 'classes')
+        with open(structure_file, "w") as json_file:
+            json_file.write(self.model.to_json())
+        self.model.save_weights(weight_file)
+        np.save(labels_file, self.le_encoder.classes_)
+
+    def __preprocess(self, dataset):
+        '''
+        Preprocess the dataset, transform the categorical labels into numbers.
+        Get word embeddings for the training data.
+        '''
+        shuffle(dataset)
+        data = [s['data'] for s in dataset]
+        labels = [s['label'] for s in dataset]
+        le_encoder = preprocessing.LabelEncoder()
+        le_encoder.fit(labels)
+        encoded_labels = le_encoder.transform(labels)
+        print('train %s intents with %s samples' % (len(set(labels)), len(data)))
+        print(collections.Counter(labels))
+        print(le_encoder.classes_)
+        vectorized_data = tokenize_and_vectorize(self.tokenizer, self.vectors, data)
+
+        # split_point = int(len(vectorized_data) * .9)
+        x_train = vectorized_data  # vectorized_data[:split_point]
+        y_train = encoded_labels  # encoded_labels[:split_point]
+
+        x_train = pad_trunc(x_train, self.model_cfg['maxlen'])
+
+        x_train = np.reshape(x_train, (len(x_train), self.model_cfg['maxlen'], self.model_cfg['embedding_dims']))
+        y_train = np.array(y_train)
+        return x_train, y_train, le_encoder
+
+    def __build_model(self, num_classes=2, type='keras'):
+        print('Build model')
+        model = Sequential()
+        optimizer_type = self.model_cfg.get('optimizer', 'adam')
+        layers = self.model_cfg.get('layers', 1)
+        for l in range(layers):
+            self.__addLayers(model, self.model_cfg)
+        model.add(Dense(num_classes))
+        model.add(Activation('softmax'))
+
+        def categorical_crossentropy_w_label_smoothing(y_true, y_pred,
+                                                       from_logits=False, label_smoothing=0):
+            y_pred = K.constant(y_pred) if not K.is_tensor(y_pred) else y_pred
+            y_true = K.cast(y_true, y_pred.dtype)
+
+            if label_smoothing is not 0:
+                smoothing = K.cast_to_floatx(label_smoothing)
+
+                def _smooth_labels():
+                    num_classes = K.cast(K.shape(y_true)[1], y_pred.dtype)
+                    return y_true * (1.0 - smoothing) + (smoothing / num_classes)
+
+                y_true = K.switch(K.greater(smoothing, 0), _smooth_labels, lambda: y_true)
+            return K.categorical_crossentropy(y_true, y_pred, from_logits=from_logits)
+
+        # model.compile(loss=categorical_crossentropy_w_label_smoothing,
+        #               metrics=['sparse_categorical_accuracy'],
+        #               optimizer=optimizer_type)
+        model.compile(loss='sparse_categorical_crossentropy',
+                      metrics=['sparse_categorical_accuracy'],
+                      optimizer=optimizer_type)
+        return model
+
+    def __addLayers(self, model, model_cfg):
+        maxlen = model_cfg.get('maxlen', 400)
+        strides = model_cfg.get('strides', 1)
+        embedding_dims = model_cfg.get('embedding_dims', 300)
+        filters = model_cfg.get('filters', 250)
+        activation_type = model_cfg.get('activation', 'relu')
+        kernel_size = model_cfg.get('kernel_size', 3)
+        hidden_dims = model_cfg.get('hidden_dims', 200)
+
+        model.add(Conv1D(
+            filters,
+            kernel_size,
+            padding='valid',
+            activation=activation_type,
+            strides=strides,
+            input_shape=(maxlen, embedding_dims)))
+        model.add(GlobalMaxPooling1D())
+        model.add(Dense(hidden_dims))
+        model.add(Activation(activation_type))
+
+    def load(self, path):
+        print(f'loading model from {path}')
+        structure_file = os.path.join(path, 'structure.json')
+        weight_file = os.path.join(path, 'weight.h5')
+        labels_file = os.path.join(path, 'classes.npy')
+        with open(structure_file, "r") as json_file:
+            json_string = json_file.read()
+
+        self.model = model_from_json(json_string)
+        self.model.load_weights(weight_file)
+        self.model._make_predict_function()
+
+        self.le_encoder = preprocessing.LabelEncoder()
+        self.le_encoder.classes_ = np.load(labels_file)
+
+    def predict(self, input: List[str]):
+        vectorized_data = tokenize_and_vectorize(self.tokenizer, self.vectors, input)
+        x_train = pad_trunc(vectorized_data, self.model_cfg['maxlen'])
+        vectorized_input = np.reshape(x_train, (len(x_train), self.model_cfg['maxlen'], self.model_cfg['embedding_dims']))
+
+        probs = self.model.predict_proba(vectorized_input)
+        preds = self.model.predict_classes(vectorized_input)
+        probs = probs.tolist()
+        results = self.le_encoder.inverse_transform(preds)
+        output = [{'input': input[i],
+                   'embeddings': x_train[i],
+                   'label': r,
+                   'highestProb': max(probs[i]),
+                   'prob': dict(zip(self.le_encoder.classes_, probs[i]))
+                   } for i, r in enumerate(results)]
+        return output
